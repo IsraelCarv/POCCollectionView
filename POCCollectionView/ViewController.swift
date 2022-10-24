73e3f00e315e8cc19dce5df58c5a10cd3ab79c86
Rael <israelcarvalhosantos@gmail.com>
2022-10-24 09:52:39 -0300

    Initial Commit

---
+++ POCCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  POCCollectionView
+//
+//  Created by Israel Carvalho on 24/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
